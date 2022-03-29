f24975bd76b24010d13baea93c598b70cd898d87
magantus@googlemail.com <Extrem93!>
2022-03-30 01:00:21 +0200

    init spring

---
+++ src/test/java/fh/dortmund/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.dortmund.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
